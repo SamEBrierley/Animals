e3aec9044e0b424d606f2fc3d4dbcb9a4ddf2731
Sam Ellis Brierley <samellisbrierley@gmail.com>
2022-01-24 17:18:43 +0000

    Added dependencies to the Pom

--- src/main/java/com/tsi/sam/brierley/animals/Cat.java
+++ src/main/java/com/tsi/sam/brierley/animals/Cat.java
@@ -2,13 +2,22 @@ package com.tsi.sam.brierley.animals;
 
 public class Cat extends Mammal{
     /////////////////////////Attributes//////////////////////////////////
+    private String name;
+    private String catType;
+    //private int weight;
+    //private int age;
 
 
 
 
 
     //////////////////////////////Constructors/////////////////////////
-
+    public Cat(){
+        name = "Elsie";
+        catType = "Siamese";
+        setWeight(1000);
+        setAge(5);
+    }
 
 
 
--- src/main/java/com/tsi/sam/brierley/animals/Main.java
+++ src/main/java/com/tsi/sam/brierley/animals/Main.java
@@ -2,7 +2,6 @@ package com.tsi.sam.brierley.animals;
 
 public class Main {
     public static void main (String[] args) {
-        Cat cat = new Cat();
-        System.out.println(cat.breathe());
+
     }
 }
