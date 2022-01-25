a1416f3dd011f4fa0dbbe430593a551790511443
Sam Ellis Brierley <samellisbrierley@gmail.com>
2022-01-25 12:53:52 +0000

    Removed unused code and classes

--- src/main/java/com/tsi/sam/brierley/animals/DomesticCat.java
+++ src/main/java/com/tsi/sam/brierley/animals/DomesticCat.java
@@ -4,7 +4,6 @@ public class DomesticCat extends Cat {
     ////////////////////////Attributes////////////////////////
     private String petCatName;
 
-
     ///////////////////////Constructor////////////////////////
     public DomesticCat(){
         petCatName = "Dennis";
