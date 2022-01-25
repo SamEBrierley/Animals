f961972897627228dc50d63049d637b7a35892d4
Sam Ellis Brierley <samellisbrierley@gmail.com>
2022-01-25 11:47:18 +0000

    Removed unused code

--- src/main/java/com/tsi/sam/brierley/animals/Main.java
+++ src/main/java/com/tsi/sam/brierley/animals/Main.java
@@ -2,8 +2,9 @@ package com.tsi.sam.brierley.animals;
 
 public class Main {
     public static void main (String[] args) {
+        /*
         DomesticCat petCat = new DomesticCat();
         petCat.setDomesticCatName("Elsie");
-
+        */
     }
 }
